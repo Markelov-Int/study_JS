d1c45e63b05adb7d717e9e4b0766dd383ba6cf79
Andrey <dlarabotbot@mail.ru>
2024-10-17 13:53:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// let obj = {
+//     name: "John",
+//     age: 15,
+//     isMarried: false
+// };
+
+// obj["b"] = "he";
+
+// console.log(obj.b)
+// console.log(obj['b'])
+
+//////////////////// ______alert__________///////
+
+//alert(1)
+
+// const result = confirm("Are u here?")
+// console.log(result)
+
+// const answer = +prompt("Do u have 18 e o?", "18");
+// console.log(answer + 6);
+
+// let answer = [];
+
+// answer[0] = prompt("Ваше имя?", "")
+// answer[1] = prompt("Ваша фамилия?", "")
+// answer[2] = +prompt("Сколько вам лет?", "")
+
+// console.log(typeof(answer));
+
+/////////////////////////////////// ИНТЕРПОЛЯЦИЯ /////////////////
+
+// let a = "John";
+
+// alert(`Привет, ${a}`);
+
+
+///////////////////////////////////// ИНКРЕМЕНТ-ДЕКРЕМЕНТ //////////////
+
+let a = 10;
+console.log(a++);
+console.log(a);
